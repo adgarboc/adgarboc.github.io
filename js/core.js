cbc6f541adc8c6a13916ca482965894b7e5113fb
adgarboc <adgarboc@gmx.com>
2020-03-12 14:07:11 -0600

    Add toggle functionaly

--- js/core.js
+++ js/core.js
@@ -1,4 +1,36 @@
-function myFunction() {
+
+changeLanguaje(0)
+
+
+function myFunction() 
+{
     var element = document.body;
     element.classList.toggle("dark-mode");
-  } 
+}
+function changeLanguaje(lang) 
+{
+    let es = document.getElementsByClassName('es');
+    let en = document.getElementsByClassName('en');
+    switch (lang) 
+    {
+        case 0:
+            for (let i = 0; i < en.length; i++) 
+            {
+                en[i].style.display = 'block';
+                es[i].style.display = 'none';
+            }
+            console.log('Hello!');
+            break;
+        case 1:
+            for (let i = 0; i < es.length; i++) 
+            {
+                en[i].style.display = 'none';
+                es[i].style.display = 'block';
+            }
+            console.log('Hola!');
+            break;
+        default:
+            break;
+    }
+
+}
